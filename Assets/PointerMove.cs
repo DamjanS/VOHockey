de90575d4064349e6b59163196e7091d47ee66e7
Tomislav <smrecki.tomislav@gmail.com>
2014-12-22 20:12:45 +0100

    CircleColliders & RigidyBody with 0 gravity effect

---
+++ Assets/PointerMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		
+		transform.position = new Vector3 (ray.GetPoint (10f).x, ray.GetPoint (10f).y, transform.position.z);
+	}
+}
