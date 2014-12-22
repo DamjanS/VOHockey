259f32b4ba8020e660bcac0c268801329328593f
Tomislav <smrecki.tomislav@gmail.com>
2014-12-22 21:50:47 +0100

    Scene mody

---
+++ Assets/PuckMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuckMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.AddForce (rigidbody2D.velocity.normalized * 1, ForceMode2D.Impulse);
+	}
+
+	void FixedUpdate() {
+		rigidbody2D.velocity = rigidbody2D.velocity.normalized * 1;
+		rigidbody2D.AddForce (rigidbody2D.velocity.normalized * 1, ForceMode2D.Impulse);
+		}
+
+}
