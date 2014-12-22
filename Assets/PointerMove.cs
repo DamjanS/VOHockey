0e4466fe7659ac873fd2216ca8de09a87ab7e31b
Damjan Spevec <damjan.spevec@gmail.com>
2014-12-22 22:29:18 +0100

    Popravak table

--- Assets/PointerMove.cs
+++ Assets/PointerMove.cs
@@ -11,7 +11,7 @@ public class PointerMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-
+		
 		transform.position = new Vector3 (ray.GetPoint (10f).x, ray.GetPoint (10f).y, transform.position.z);
 	}
 }
