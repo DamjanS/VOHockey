403bec9cedb65db661a31c116e9a67613fdf78c7
Filip Čajko <filip.cajko@gmail.com>
2014-12-24 10:03:43 +0100

    lala

---
+++ Assets/PointerPlayer1Move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointerPlayer1Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update (){
+
+		var tapCount = Input.touchCount;
+		for ( var i = 0 ; i < tapCount ; i++ ) {
+			var touch = Input.GetTouch(i);
+			if(touch.position.y<Screen.height/2){
+				Debug.Log("Pozicija: X:"+touch.position.x+", Y:"+touch.position.y);
+				Ray ray = Camera.main.ScreenPointToRay (touch.position);
+				transform.position = new Vector3 (transform.position.x, ray.GetPoint(10f).y, transform.position.z);
+			}
+		}
+
+	}
+}
---
+++ Assets/PointerPlayer2Move.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointerPlayer2Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var tapCount = Input.touchCount;
+		for ( var i = 0 ; i < tapCount ; i++ ) {
+			var touch = Input.GetTouch(i);
+			if(touch.position.y>Screen.height/2){
+				Debug.Log("Pozicija: X:"+touch.position.x+", Y:"+touch.position.y);
+				Ray ray = Camera.main.ScreenPointToRay (touch.position);
+				transform.position = new Vector3 (transform.position.x, ray.GetPoint(10f).y, transform.position.z);
+			}
+		}
+		/*int randomNumber = Random.Range (1, 10);
+
+			float randMove = Random.Range(-10.0f, 10.0f);
+						float speed = 4.0f;
+			Vector3 random = new Vector3();
+						random.x = 0;
+			random.y = randMove;
+			random.z = 0;
+			if (gameObject.transform.position.y > 2 && random.y > 0) random.y *= -1;
+			else if (gameObject.transform.position.y < -2 && random.y < 0) random.y *= -1;
+			transform.Translate (random * Time.deltaTime);
+				*/
+	}
+}
